ce246c6b05a8c10d084243eb7524b8a4bb3497d8
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-07-11 13:45:52 +0900

    sct3-32 라우터 요청

--- sct3-basic/app.js
+++ sct3-basic/app.js
@@ -1,9 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
-
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Context-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><titleEnter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Context-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
