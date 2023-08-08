1ec8d3b053bc60ad02e1534bf5f130873ba8c4f8
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-08-08 10:57:28 +0900

    sct11 더미 사용자 생성 및 관리하기

--- 06-adding-a-one-to-many-relationship/app.js
+++ 06-adding-a-one-to-many-relationship/app.js
@@ -19,6 +19,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -30,7 +39,17 @@ User.hasMany(Product);
 sequelize
   .sync({ force: true })
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({ name: "Max", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
