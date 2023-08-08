4cc4be4b4d4b76dda45ac73591fbcab22ca0c2a4
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-08-08 15:48:38 +0900

    sct12 장바구니에 여러 제품 저장하기

--- 09-displaying-the-cart-items/app.js
+++ 09-displaying-the-cart-items/app.js
@@ -1,3 +1,4 @@
+// app.js
 const path = require("path");
 
 const express = require("express");
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("64d1ded4571987762db54be3")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
